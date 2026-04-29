ba60d7291b8e930d89fce1d274e28b0a70d5aa9e
Applejuicelolmc <dekortbrian1@gmail.com>
2026-04-29 09:48:35 +0200

    feat: remove unused build script for Docker image

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Builds the syn-tool Docker image.
-# Uses --security-opt seccomp=unconfined because Synology's kernel does not
-# support seccomp, which causes all RUN steps to fail with the default profile.
-docker build --security-opt seccomp=unconfined -t syn-tool .
